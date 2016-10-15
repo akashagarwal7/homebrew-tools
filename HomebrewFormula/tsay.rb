2cdd27f8b5351fd1f415fe6086d47b90d6dac25a
Akash Aggarwal <virusofminds@gmail.com>
2016-10-15 20:26:11 +0530

    Updated sha

--- HomebrewFormula/tsay.rb
+++ HomebrewFormula/tsay.rb
@@ -3,7 +3,7 @@ class Tsay < Formula
   homepage "https://github.com/akashaggarwal7/tsay"
   url "https://github.com/akashaggarwal7/tsay/archive/1.0.2.tar.gz"
   version "1.0.2"
-  sha256 "9902f721e35bd21534474518742cdbad9c408e9a4f0311933ea3c728253e79ca"
+  sha256 "4d9b286e0af1be5528f91970b4674dca296f31c33def95f8b0a3677a21159e5e"
   def install
     bin.install "tsay"
   end

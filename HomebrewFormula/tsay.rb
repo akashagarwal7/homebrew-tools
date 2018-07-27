0443126d78d5f8126757e17b0ed3819e5ae5be01
Akash Agarwal <virusofminds@gmail.com>
2018-07-27 18:26:49 +1000

    Fix username

--- HomebrewFormula/tsay.rb
+++ HomebrewFormula/tsay.rb
@@ -1,7 +1,7 @@
 class Tsay < Formula
   desc ""
-  homepage "https://github.com/akashaggarwal7/tsay"
-  url "https://github.com/akashaggarwal7/tsay/archive/1.0.2.tar.gz"
+  homepage "https://github.com/akashagarwal7/tsay"
+  url "https://github.com/akashagarwal7/tsay/archive/1.0.2.tar.gz"
   version "1.0.2"
   sha256 "4d9b286e0af1be5528f91970b4674dca296f31c33def95f8b0a3677a21159e5e"
   def install
